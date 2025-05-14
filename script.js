e58a9c099c23bc0b2c5aac44dc1bc04b377a8aaf
Tauqeer Rahim <tauqeer.rahim03@gmail.com>
2025-05-14 23:56:15 +0530

    made the website more responsive, especially on mobile and added a file and meta tags to avoid warnings by google

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ const questions = document.querySelectorAll(".faq-question");
 questions.forEach((question) => {
     question.addEventListener("click", () => {
         const answer = question.nextElementSibling;
+        const icon = question.querySelector(".plus-icon");
 
-        // Close all other answers
+        // Close all other answers and reset icons
         document.querySelectorAll(".faq-answer").forEach((a) => {
             if (a !== answer) a.style.maxHeight = null;
         });
+        document.querySelectorAll(".plus-icon").forEach((i) => {
+            if (i !== icon) i.classList.remove("rotate");
+        });
 
-        // Toggle current one
+        // Toggle current answer and icon rotation
         if (answer.style.maxHeight) {
             answer.style.maxHeight = null;
+            icon.classList.remove("rotate");
         } else {
             answer.style.maxHeight = answer.scrollHeight + "px";
+            icon.classList.add("rotate");
         }
     });
 });
